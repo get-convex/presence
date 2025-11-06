060ca5e1b7372095e9ccd8d708a9450bb3bf2f7f
Ian Macartney <ianhmacartney@gmail.com>
2025-11-05 23:25:17 -0800

    update codegen

--- example-with-auth/src/main.tsx
+++ example-with-auth/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
 
 const address = import.meta.env.VITE_CONVEX_URL;
